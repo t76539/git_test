422d1adde13f94840a50b1b3ef41e4afa6678d1e
a-samodov <a-samodov@grand-prix.ru>
2017-08-07 11:18:46 +0300

    New printn

--- src/git_test/Main.java
+++ src/git_test/Main.java
@@ -3,8 +3,8 @@ package git_test;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello, world!!!");
+		System.out.println("Test1");
 	}
 
 }
