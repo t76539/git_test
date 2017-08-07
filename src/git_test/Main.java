ae401a63de0699cafc5010ec258acfcdcc68a211
a-samodov <a-samodov@grand-prix.ru>
2017-08-07 12:42:19 +0300

    b3 - print

--- src/git_test/Main.java
+++ src/git_test/Main.java
@@ -5,6 +5,9 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("HI-START");
     b2();
+    
+    b3 b = new b3();    
+    System.out.println(b.name);    
   }
   
   private static void b2() {
